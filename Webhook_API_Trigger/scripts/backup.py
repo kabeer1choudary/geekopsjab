faa51a9d45e6cce1c43bbfe0d7a4847abfb42b2a
kabeer1choudary <kabeer.choudary@gmail.com>
2026-06-15 18:38:04 +0530

    update scripts

--- Webhook_API_Trigger/scripts/backup.py
+++ Webhook_API_Trigger/scripts/backup.py
@@ -1,9 +1,2 @@
-# deploy_app.py
-import sys
-
-print("Hello from the triggered script!")
-
-# Retrieve the payload passed from the server
-if len(sys.argv) > 1:
-    payload_received = sys.argv[1]
-    print(f"Received payload data: {payload_received}")
+#backup
+print("Hello from the backup script!")
